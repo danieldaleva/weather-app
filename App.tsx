110b6c4a62f34df9c937f26a7d365bb28407fdbd
Daniel Costa <dc@Nankin.local>
2021-11-04 12:23:09 -0300

    code review

--- App.tsx
+++ App.tsx
@@ -13,10 +13,7 @@ import { enableScreens } from 'react-native-screens';
 enableScreens(true);
 
 const App: React.FC = () => {
-  const backgroundColor = useThemeColor(
-    'background',
-    null,
-  ) as unknown as string;
+  const backgroundColor = useThemeColor('background', null) as string;
 
   return (
     <AppContextProvider>
